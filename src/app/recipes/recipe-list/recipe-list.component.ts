f7ee4d74df7ab94c46637c96d6a9d7a42360a10e
mklova <mklova@techmatesgroup.com>
2024-04-08 13:55:53 +0300

    commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
 
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('ЗЕЛЕНИЙ БОРЩ', 'РЕЦЕПТ ЗІ СВІЖИМ ШПИНАТОМ', 'https://klopotenko.com/wp-content/uploads/2022/04/zelenyy-borshch-zi-shpynatom-img-1000x600.jpg?v=1689073582'),
+    new Recipe('ПУНДИКИ', 'ЗІ ЗГУЩЕНИМ МОЛОКОМ ТА ГОРІХАМИ', 'https://klopotenko.com/wp-content/uploads/2023/11/pundyky-z-gorihamy-ta-zgushenym-molokom-img-1000x600.jpg?v=1701174403')
+  ];
+
+  constructor(){}
+  ngOnInit(){
+
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingridient [] = [
+    new Ingridient ('apples', 5),
+    new Ingridient ('tomatoes', 10)
+  ];
 
+  constructor(){}
+  ngOnInit(){
+  }
 }
